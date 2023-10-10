91e5fd7119320adc7676bae146ca8de9e1fa1b54
Oleksandr Tereshchenko <toporok@gmail.com>
2023-10-10 09:34:24 +0300

    Initial models (#1)
    * cacheable
    
    * cacheable
    
    * cacheable
    
    * cacheable

---
+++ agentcache/models.py
@@ -0,0 +1,22 @@
+"""TODO oleksandr"""
+import hashlib
+
+from pydantic import BaseModel
+
+
+class Cacheable(BaseModel):
+    """A base class for objects that can be cached."""
+
+    class Config:
+        """Pydantic config."""
+
+        frozen = True
+
+    @property
+    def hash_key(self) -> str:
+        """Get the cache key for this object. The cache key is a hash of the JSON representation of the object."""
+        if not hasattr(self, "_hash_key"):
+            # pylint: disable=attribute-defined-outside-init
+            # noinspection PyAttributeOutsideInit
+            self._hash_key = hashlib.sha256(self.model_dump_json().encode("utf-8")).hexdigest()
+        return self._hash_key
