6633b7b78db7d70250b242b1966f59c246537e56
Oleksandr Tereshchenko <toporok@gmail.com>
2023-10-11 10:30:35 +0300

    openai

--- agentcache/ext/llms/openai.py
+++ agentcache/ext/llms/openai.py
@@ -4,7 +4,7 @@ from typing import List
 from agentcache.models import Message
 
 
-async def achatgpt(messages: List[Message], stream=False, **kwargs):
+async def achatgpt(messages: List[Message], stream: bool = False, **kwargs):
     """TODO Oleksandr"""
     import openai  # pylint: disable=import-outside-toplevel
 
