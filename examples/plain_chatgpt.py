9ff1bd510cd2487eb8c6f9b5791c7930a0f42ce2
Oleksandr Tereshchenko <toporok@gmail.com>
2023-10-15 20:27:19 +0300

    minor

--- examples/plain_chatgpt.py
+++ examples/plain_chatgpt.py
@@ -9,34 +9,23 @@ load_dotenv()
 from agentcache.agents import AgentFirstDraft
 
 
-async def main(echo_input: bool = False) -> None:
+async def main() -> None:
     """The chat loop."""
     agent = AgentFirstDraft()
     try:
         while True:
-            print()
-            user_input = custom_input("YOU: ", echo_input=echo_input)
-            print()
-            print("GPT: ", end="", flush=True)
+            user_input = input("\nYOU: ")
+            if user_input == "exit":
+                raise KeyboardInterrupt
+
             response = await agent.arun(user_input, model="gpt-3.5-turbo-0613", stream=True)
+
+            print("\nGPT: ", end="", flush=True)
             async for token in response:
                 print(token.text, end="", flush=True)
             print()
     except KeyboardInterrupt:
-        pass
-    print()
-
-
-def custom_input(prompt_text: str, echo_input: bool = False) -> str:
-    """Custom input function that allows to exit the chat by typing "exit"."""
-    user_input = input(prompt_text)
-    if user_input == "exit":
-        raise KeyboardInterrupt("exit")
-    if echo_input:
-        # helps to visualise it in a jupyter notebook, because in a notebook the input happens in a popup
         print()
-        print(prompt_text + user_input)
-    return user_input
 
 
 if __name__ == "__main__":
