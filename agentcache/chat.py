8a7895b009550ca2b93e92ce7ac3bacf8944e3e7
Oleksandr Tereshchenko <toporok@gmail.com>
2023-10-15 14:12:19 +0300

    MessageBundle

--- agentcache/chat.py
+++ agentcache/chat.py
@@ -1,11 +1,26 @@
-from typing import List
+from asyncio import Queue
+from typing import List, Optional, Union
 
 from agentcache.typing import MessageType
+from agentcache.utils import END_OF_QUEUE
 
 
 class MessageBundle:
     def __init__(self) -> None:
         self.messages_so_far: List[MessageType] = []
+        # pylint: disable=unsubscriptable-object
+        self._message_queue: Optional[Queue[Union[MessageType, object]]] = Queue()
+
+    async def asend_message(self, message: MessageType, close_bundle: bool) -> None:
+        self._message_queue.put_nowait(message)
+        if close_bundle:
+            self._message_queue.put_nowait(END_OF_QUEUE)
+
+    async def asend_interim_message(self, message: MessageType) -> None:
+        await self.asend_message(message, close_bundle=False)
+
+    async def asend_final_message(self, message: MessageType) -> None:
+        await self.asend_message(message, close_bundle=True)
 
 
 class ChatManager:
--- examples/plain_chatgpt.py
+++ examples/plain_chatgpt.py
@@ -10,32 +10,34 @@ from agentcache.models import Message
 from agentcache.ext.llms.openai import achatgpt
 
 
-async def main(print_user_input_again: bool = False) -> None:
+async def main(echo_input: bool = False) -> None:
     """The chat loop."""
-    messages = []
-    while True:
-        print()
-        prompt_text = "YOU: "
-        user_input = input(prompt_text)
-        if user_input == "exit":
-            break
-        if print_user_input_again:
-            # helps to visualise it in a jupyter notebook, because in a notebook the input happens in a popup
+    try:
+        messages = []
+        while True:
             print()
-            print(prompt_text + user_input)
-        messages.append(
-            Message(
-                role="user",
-                content=user_input,
-            )
-        )
+            messages.append(Message(role="user", content=custom_input("YOU: ", echo_input=echo_input)))
+            print()
+            print("GPT: ", end="", flush=True)
+            response = await achatgpt(messages=messages, model="gpt-3.5-turbo-0613", stream=True)
+            async for token in response:
+                print(token.text, end="", flush=True)
+            messages.append(response.get_full_message())
+            print()
+    except KeyboardInterrupt:
+        pass
+
+
+def custom_input(prompt_text: str, echo_input: bool = False) -> str:
+    """Custom input function that allows to exit the chat by typing "exit"."""
+    user_input = input(prompt_text)
+    if user_input == "exit":
+        raise KeyboardInterrupt("exit")
+    if echo_input:
+        # helps to visualise it in a jupyter notebook, because in a notebook the input happens in a popup
         print()
-        print("GPT: ", end="", flush=True)
-        response = await achatgpt(messages=messages, model="gpt-3.5-turbo-0613", stream=True)
-        async for token in response:
-            print(token.text, end="", flush=True)
-        messages.append(response.get_full_message())
-        print()
+        print(prompt_text + user_input)
+    return user_input
 
 
 if __name__ == "__main__":
