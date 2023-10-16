49ed748069353cf9196ccb3bbb58b6b485377a6e
Oleksandr Tereshchenko <toporok@gmail.com>
2023-10-16 13:34:41 +0300

    minor

--- agentcache/models.py
+++ agentcache/models.py
@@ -31,7 +31,6 @@ class Immutable(BaseModel):
     def hash_key(self) -> str:
         """Get the hash key for this object. It is a hash of the JSON representation of the object."""
         if not hasattr(self, "_hash_key"):
-            # TODO Oleksandr: should the json dump also include the class name ?
             # pylint: disable=attribute-defined-outside-init
             # noinspection PyAttributeOutsideInit
             self._hash_key = hashlib.sha256(self.model_dump_json().encode("utf-8")).hexdigest()
