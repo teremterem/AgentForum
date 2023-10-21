15327684f3a62a84e4ddd393c78dc26534fe84d5
Oleksandr Tereshchenko <toporok@gmail.com>
2023-10-21 18:47:46 +0300

    refactor message abstractions (in progress)

--- agentcache/agents.py
+++ agentcache/agents.py
@@ -3,16 +3,20 @@ from typing import Union
 
 from agentcache.ext.llms.openai import aopenai_chat_completion
 from agentcache.model_wrappers import MessageSequence, StreamedMessage
-from agentcache.models import Message
+from agentcache.models import Message, Freeform
 
 
 async def acall_agent_draft(request: Union[StreamedMessage, Message], **kwargs) -> MessageSequence:
     """The very first agent."""
-    # TODO Oleksandr: a bundle of messages that are not part of the same conversation doesn't make sense
-    # TODO Oleksandr: if we want to cache the agent response then all incoming messages should be known before the
-    #  agent function is entered
+    requests = MessageSequence(  # TODO Oleksandr: replace this with AgentCall
+        sequence_metadata=Freeform(**kwargs),
+        items_so_far=[request],
+        completed=True,
+    )
     message = (await request.aget_all())[-1]
-    response = await aopenai_chat_completion(messages=await message.aget_full_chat(), kwargs=request.sequence_metadata)
+    response = await aopenai_chat_completion(
+        messages=await message.aget_full_chat(), kwargs=requests.sequence_metadata
+    )
     return MessageSequence(items_so_far=[response], completed=True)
 
 
--- examples/plain_chatgpt.py
+++ examples/plain_chatgpt.py
@@ -1,47 +1,45 @@
 # pylint: disable=wrong-import-position
 """Chat with OpenAI ChatGPT using the AgentCache library."""
 import asyncio
+from typing import Optional
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-from agentcache.agents import afirst_agent
-from agentcache.model_wrappers import MessageSequence
-from agentcache.models import Freeform
+from agentcache.agents import acall_agent_draft
+from agentcache.model_wrappers import StreamedMessage
+from agentcache.models import Message
 from agentcache.storage import InMemoryStorage
 
 
 async def main() -> None:
     """The chat loop."""
     forum = InMemoryStorage()
-    message = None
+    latest_message: Optional[StreamedMessage] = None
     try:
         while True:
             user_input = input("\nYOU: ")
             if user_input == "exit":
                 raise KeyboardInterrupt
 
-            if not message:
-                message = await message_tree.anew_message(content=user_input)
-            else:
-                message = await message.areply(content=user_input)
-            requests = MessageSequence(  # TODO Oleksandr: move this to the framework level
-                sequence_metadata=Freeform(
-                    model="gpt-3.5-turbo-0613",
-                    stream=True,
-                ),
-                items_so_far=[message],
-                completed=True,
+            latest_message = StreamedMessage(  # TODO Oleksandr: move this to the framework level
+                forum=forum,
+                full_message=Message(content=user_input),
+                reply_to=latest_message,
             )
-            responses = await afirst_agent(requests)
+            responses = await acall_agent_draft(
+                request=latest_message,
+                model="gpt-3.5-turbo-0613",
+                stream=True,
+            )
 
             async for streamed_message in responses:
                 print("\nGPT: ", end="", flush=True)
                 async for token in streamed_message:
                     print(token.text, end="", flush=True)
                 print()
-            message = await (await responses.aget_all())[-1].aget_full_message()
+            latest_message = (await responses.aget_all())[-1]  # TODO Oleksandr: aget_concluding_message()
     except KeyboardInterrupt:
         print()
 
