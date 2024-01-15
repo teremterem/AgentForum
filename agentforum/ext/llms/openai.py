37747fbbdda4e5585f94e67c68aad6e331c9d596
Oleksandr Tereshchenko <toporok@gmail.com>
2024-01-15 08:15:54 +0200

    minor: edited a todo

--- agentforum/ext/llms/openai.py
+++ agentforum/ext/llms/openai.py
@@ -57,8 +57,8 @@ async def _message_to_openai_dict(message: Union[MessagePromise, Message, Dict[s
     if isinstance(message, Message):
         # TODO Oleksandr: introduce a lambda function to derive roles from messages ?
         try:
-            # TODO Oleksandr: should `openai_role` take precedence over `role` ? should there even be such a thing as
-            #  `openai_role` ?
+            # TODO TODO TODO Oleksandr: should `openai_role` take precedence over `role` ? should there even be such
+            #  a thing as `openai_role` ?
             role = message.openai_role
         except AttributeError:
             role = getattr(message, "role", "user")
