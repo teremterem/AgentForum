4673695e6d7d22df23d8cffd159a8b9aeec0cdfb
Oleksandr Tereshchenko <toporok@gmail.com>
2024-01-16 10:17:15 +0200

    define custom typing annotations unconditionally

--- agentforum/typing.py
+++ agentforum/typing.py
@@ -7,8 +7,8 @@ from agentforum.forum import InteractionContext
 from agentforum.models import Message
 from agentforum.promises import StreamedMessage, MessagePromise
 
-SingleMessageType = Union[str, StreamedMessage, Message, MessagePromise, BaseException]
 AgentFunction = Callable[[InteractionContext, ...], Awaitable[None]]
 
+SingleMessageType = Union[str, StreamedMessage, Message, MessagePromise, BaseException]
 # TODO Oleksandr: why not allow Iterable and AsyncIterable of MessageType itself ?
 MessageType = Union[SingleMessageType, Iterable[SingleMessageType], AsyncIterable[SingleMessageType]]
