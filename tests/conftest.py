474b502662c1de6df6c135a978868880ff65f1ad
Oleksandr Tereshchenko <toporok@gmail.com>
2024-01-22 10:02:50 +0200

    minor

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,17 @@ from agentforum.forum import Forum, InteractionContext
 
 @pytest.fixture
 def forum() -> Forum:
-    """Create a forum with in-memory storage."""
+    """
+    Create a forum with in-memory storage.
+    """
     return Forum()
 
 
 @pytest.fixture
 def fake_interaction_context(forum: Forum) -> None:  # pylint: disable=redefined-outer-name
-    """Create a fake interaction context."""
+    """
+    Create a fake interaction context.
+    """
     # TODO TODO TODO Oleksandr: we shouldn't need this fixture as soon as aflatten_message_sequence() becomes
     #  independent of Forum
     # noinspection PyTypeChecker
